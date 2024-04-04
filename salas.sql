d0d454aed8dee192cf87d03e865248bb98f7eda2
Rafaella <133260227+rafaella489@users.noreply.github.com>
2024-04-04 09:43:28 -0300

    Create salas.sql

---
+++ salas.sql
@@ -0,0 +1,29 @@
+CREATE TABLE ENTRETERIMENTO (SALA VARCHAR(100), AR COND. VARCHAR(100), QUANTIDADE VARCHAR(100) );
+   
+    -- Script para inserir dados na tabela SALAS
+   
+    INSERT INTO ENTRETERIMENTO (SALA, AR COND., QUANTIDADE) VALUES
+    ('Automação 4.0', 'tem', '2' ),
+    ('Analista de Qualidade de Vida', 'tem', '1' ),
+    ('Cantina', 'tem', '2' ),
+    ('Coordenação', 'tem', '1' ),
+    ('Diretoria', 'tem', '1' ),
+    ('De mont. e desm. de rolamentos', 'tem', '1' ),
+    ('Espaço Educação e Cultural', '', '1' ),
+    ('Eletrônica', 'tem', '1' ),
+    ('Instalações Elétricas', 'tem', '1' ),
+    ('Laboratório de Informática', 'tem', '1' ),
+    ('Laboratório de TI-01 des', 'tem', '1' ),
+    ('Laboratório Hidráulica', 'tem', '2' ),
+    ('Laboratório Comando de Máquinas Elétricas', 'tem', '1' ),
+    ('Laboratório CAD/CAM', 'tem', '1' ),
+    ('Laboratório TI 03', 'tem', '1' ),
+    ('Laboratório TI 02 AI', 'tem', '1' ),
+    ('Metrologia', 'tem', '1' ),
+    ('Orientador de prática profissional', 'tem', '1' ),
+    ('Pneumática', 'tem', '1' ),
+    ('Relacionamento com Empresa', 'tem', '2' ),
+    ('Sala de estar', 'tem', '2' ),
+    ('Sala de aula 04', 'tem', '2' ),
+    ('Sala de aula 05', 'tem', '2' ),
+    ('Secretaria', 'tem', '2' )
